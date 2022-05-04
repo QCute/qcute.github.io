0b87e68494e2ffa36bd97f9b579a3ebb3648d244
QCute <QCute@users.noreply.github.com>
2022-05-04 06:51:22 +0000

    deploy: c57ac748fd8cd36b999d5ae840e6e1407d2a9780

--- js/graphite.js
+++ js/graphite.js
@@ -1,44 +1,28 @@
 'use strict';
 
 // back-to-top
-$(document).ready((function (_this) {
-  return function () {
-    let bt
-    bt = $('#back_to_top')
-    if ($(document).width() > 480) {
-      $(window).scroll(function () {
-        let st
-        st = $(window).scrollTop()
-        if (st > 30) {
-          return bt.css('display', 'block')
-        } else {
-          return bt.css('display', 'none')
-        }
-      })
-      return bt.click(function () {
-        $('body,html').animate({
-          scrollTop: 0,
-        }, 800)
-        return false
-      })
+window.onload = () => {
+  let element = document.querySelector('#back_to_top');
+  if(!element) return;
+  var elevator = new Elevator({ 'element': element, 'duration': 800 });
+  if (document.body.clientWidth > 480) {
+    window.onscroll = () => {
+      if(window.scrollY > 30) {
+        element.style.display = 'block';
+      } else {
+        element.style.display = 'none';
+      }
     }
   }
-})(this))
+}
 
-// nav-toggle
-$(document).ready((function (_this) {
-  return function () {
-    let nav,icon
-    icon = $('#menu_icon')
-    nav = $('#site_nav')
-    icon.click(function () {
-      nav.slideToggle(250)
-    })
-  }
-})(this))
+// Menu Toggle
+document.querySelector("#menu_icon").onclick = () => {
+  domSlider.slideToggle({'element': document.querySelector("#site_nav")})
+};
 
 // FancyBox
-$('[data-fancybox="gallery"]').fancybox({
+Fancybox.bind("[data-fancybox='gallery']", {
   arrows: false,
   infobar: false,
   buttons: [],
